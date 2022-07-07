fbec60737388b14f2343075c7c0883a25fe5ce07
Shimizu <51806272+kasuuuu4423@users.noreply.github.com>
2022-07-07 10:13:16 +0900

    Delete data directory

--- data/programs/components/BtnPref.js
+++
@@ -1,9 +0,0 @@
-const BtnPref = () =>{
-    const btn = $(".btn-ts");
-    btn.on("click", function(){
-        const modal = $(this).siblings(".dropdown-menu");
-        modal.toggle();
-    });
-}
-
-export default BtnPref;
